85861fa8829d72e6a90ad5c4b0091fbf5d3ec70c
ipramudya <theprimedownload@gmail.com>
2021-07-31 02:20:49 +0700

    Initial commit: Membuat Navbar, Products, Product Component

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import useStyle from './styles';
+
+const Product = ({ product }) => {
+  const classes = useStyle();
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.img}
+        title={product.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant='h5' gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant='h5'>{product.price}</Typography>
+        </div>
+        <Typography variant='body2'>{product.description}</Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label='Add to cart'>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,39 @@
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
+const products = [
+  {
+    id: 1,
+    name: 'Shoes',
+    description: 'Running Shoes',
+    price: '$5',
+    img: 'https://source.unsplash.com/jng0VFa-jRw/640x640',
+  },
+  {
+    id: 2,
+    name: 'Jacket',
+    description: 'Colorful Jacket',
+    price: '$10',
+    img: 'https://source.unsplash.com/J8wrzpD0qXg/640x640',
+  },
+];
+
+const Products = () => {
+  const classes = useStyles();
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justifyContent='center' spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
